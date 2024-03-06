0c6464b35aec5093e350dbc08db0b89c55212263
edugcravo <edugcravo@hotmail.com>
2024-03-06 03:56:56 -0300

    adicionado endpoint de listar produtos

--- router/produto/routes.py
+++ router/produto/routes.py
@@ -6,7 +6,8 @@ from config.db import engine
 from model.produto import produto
 from model.imagens import imagens
 from model.associacao import produto_cores, produto_armazenamento, produto_imagens
-
+from model.cores import cores
+from model.armazenamento import armazenamento
 
 produto_router = APIRouter(
     prefix="/produto",
@@ -63,8 +64,8 @@ async def listar_produtos():
             result = conn.execute(produto.select()).fetchall()
             produtos = []
 
-            # pegar as imagens, cores e armazenamentos e adicionar ao produto
             for item in result:
+                # Buscar as imagens associadas ao produto
                 imagens_produto = conn.execute(produto_imagens.select().where(produto_imagens.c.produto_id == item.id)).fetchall()
                 imagens_produtos = []
                 for img in imagens_produto:
@@ -73,11 +74,19 @@ async def listar_produtos():
                     if len(imagens_produtos) >= 4:
                         break
 
+                # Buscar as cores associadas ao produto
                 cores_produto = conn.execute(produto_cores.select().where(produto_cores.c.produto_id == item.id)).fetchall()
-                cores_produtos = [cores.cores_id for cores in cores_produto]
+                cores_produtos = []
+                for cor in cores_produto:
+                    cor_data = conn.execute(cores.select().where(cores.c.id == cor.cores_id)).fetchone()
+                    cores_produtos.append(cor_data.nome)
 
-                armazenamentos_produto = conn.execute(produto_armazenamento.select().where(produto_armazenamento.c.produto_id == item.id)).fetchall()
-                armazenamentos_produtos = [armazenamento.armazenamento_id for armazenamento in armazenamentos_produto]
+                # Buscar o armazenamento associado ao produto
+                armazenamento_produto = conn.execute(produto_armazenamento.select().where(produto_armazenamento.c.produto_id == item.id)).fetchall()
+                armazenamento_produtos = []
+                for arm in armazenamento_produto:
+                    arm_data = conn.execute(armazenamento.select().where(armazenamento.c.id == arm.armazenamento_id)).fetchone()
+                    armazenamento_produtos.append(arm_data.quantidade)
 
                 produtos.append({
                     'id': item.id,
@@ -90,11 +99,13 @@ async def listar_produtos():
                     'miniatura': item.miniatura,
                     'imagens': imagens_produtos,
                     'cores': cores_produtos,
-                    'armazenamentos': armazenamentos_produtos
+                    'armazenamento': armazenamento_produtos
                 })
 
             return {'status': 200, 'produtos': produtos}
         except Exception as e:
             print(f"Erro ao listar produtos: {e}")
             return {'status': 500, 'message': 'Erro ao listar produtos'}
+
+
 
